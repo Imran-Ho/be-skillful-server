03e585f45330c234d84211579dbf4f799e4aa464
Imran-Ho <imranhossain36@gmail.com>
2022-10-26 09:29:36 +0600

    fake data meking for courses

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port =process.env.PORT || 5000;
 
+app.use(cors())
+
+const courses = require('./Data/courses.json')
+// console.log(courses)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Be-Skillful server running')
+})
+
+app.get('/courses', (req, res) =>{
+  res.send(courses)
 })
 
 app.listen(port, () => {
